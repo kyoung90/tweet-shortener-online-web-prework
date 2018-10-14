4db187bb16dc43a60744817b073717ca23332587
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-14 20:00:07 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+def dictionary(str)
+  str.split(" ")
+  code = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "$"
+  }
+  
+  str.each do |ele|
+    if 
+end 
